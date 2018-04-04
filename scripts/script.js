66ec1ef873af6d5b2a118c337f2578d085d17d47
Michael Oria <Michael_Oria@me.com>
2018-04-04 15:14:51 -0400

    Minor adjustments.

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,14 @@
-// var x = "Welcome to My Personal Website.";
-// alert(x);
+// script(s)
+
+function expandMe(x) { // function will enlarge images 1px when on a mouse hover.
+    x.style.height = "201px";
+    x.style.width = "201px";
+
+};
+
+function normalImg(x) { // function will set 
+    x.style.height = "200px";
+    x.style.width = "200px";
+
+};
 

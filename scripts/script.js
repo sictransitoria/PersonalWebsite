6ba403ace33dc6c4e6f6c3368f9fcbab923439bf
Michael Oria <Michael_Oria@me.com>
2018-04-11 22:56:03 -0400

    Updates with new content, responsive design, styling.

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,12 @@
-// script(s)
-
-function expandMe(x) { // function will enlarge images 1px when on a mouse hover.
-    x.style.height = "201px";
-    x.style.width = "201px";
+function navBar() {
+    var menu = document.getElementById("myTopNav");
+    
+    if (menu.className === "navigation") {
+        menu.className += " responsive";
+    
+    } else {
+        menu.className = "navigation";
+    
+    }
 
 };
-
-function normalImg(x) { // function will set 
-    x.style.height = "200px";
-    x.style.width = "200px";
-
-};
-
